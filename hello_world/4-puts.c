bae68b546278cc0dc7e7a503402ef2bf6441baff
Rebecca <rebecca.fsq011@protonmail.com>
2025-06-18 00:29:38 +0000

    adding betty style comment

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-
-int main(void)
+/**
+*main -Entry point
+*
+*Return: Always 0 (success)
+*/
+Int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
