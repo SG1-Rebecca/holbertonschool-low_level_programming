4be1c7a88f30044c068300701a6dee03a498d22c
Rebecca <rebecca.fsq011@protonmail.com>
2025-06-20 10:08:23 +0000

    printing lowercase alphabet excluding two alphabets followed by a new line

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+
+char alphabet_soup;
+for (alphabet_soup = 'a'; alphabet_soup <= 'z'; alphabet_soup++)
+{
+if (alphabet_soup != 'q' && alphabet_soup != 'e')
+putchar(alphabet_soup);
+putchar('\n');
+}
+return (0);
+}
