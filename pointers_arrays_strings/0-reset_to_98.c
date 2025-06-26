7e09a2e03a214f1f43bc0537aa5518d0ed64a39f
Rebecca <rebecca.fsq011@protonmail.com>
2025-06-26 23:38:11 +0000

    update documentation to pass Betty

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,7 @@
 #include "main.h"
 /**
-* reset_to_98: Take a pointer to an int and update its value
+* reset_to_98 - Take a pointer to an int and update its value
 * @n: pointer to int
-*
-* Description: Function that take a pointer to an int
-* and update the value its points to 98
 */
 void reset_to_98(int *n)
 {
