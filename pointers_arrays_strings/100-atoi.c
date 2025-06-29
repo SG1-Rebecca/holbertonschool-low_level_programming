738be54f2c3a9ed82be3c370a11b79f3d03f753e
Rebecca <rebecca.fsq011@protonmail.com>
2025-06-29 21:02:37 +0000

    update documentation to pass Betty

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -10,28 +10,25 @@ int _atoi(char *s)
 int result = 0;
 int sign = 1;
 int digit_found = 0;
-
-    while (*s != '\0')
-    {
-        if (*s == '-')
-        {
-           
-            sign *= -1;
-        }
-        else if (*s == '+')
-        {
-        }
-        else if (*s >= '0' && *s <= '9')
-        {
-            digit_found = 1;
-            result = result * 10 + (*s - '0');
-        }
-        else if (digit_found)
-        {
-            break;
-        }
-
-        s++;
-    }
-    return (result * sign);
+while (*s != '\0')
+{
+if (*s == '-')
+{
+sign *= -1;
+}
+else if (*s == '+')
+{
+}
+else if (*s >= '0' && *s <= '9')
+{
+digit_found = 1;
+result = result * 10 + (*s - '0');
+}
+else if (digit_found)
+{
+break;
+}
+s++;
+}
+return (result * sign);
 }
