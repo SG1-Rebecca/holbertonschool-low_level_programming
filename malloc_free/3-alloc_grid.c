840b7927b24eca961090de61845dd07d432b1b96
Rebecca <rebecca.fsq011@protonmail.com>
2025-07-11 20:51:10 +0000

    fix betty coding and documentation style

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -1,6 +1,12 @@
 #include "main.h"
 #include <stdlib.h>
-/********/
+/**
+* alloc_grid - allocate a 2 dimensional array of integers
+* @width: width of the grid
+* @height: height of the grid
+*
+* Return: pointer to the 2 dimensional array, or NULL on failure
+*/
 int **alloc_grid(int width, int height)
 {
 int **grid;
@@ -14,12 +20,12 @@ if (grid == NULL)
 {
 return (NULL);
 }
-for(i = 0; i < height; i++)
+for (i = 0; i < height; i++)
 {
-grid[i]= malloc(sizeof(int) * width);
+grid[i] = malloc(sizeof(int) * width);
 if (grid[i] == NULL)
 {
-for(i--; i >= 0; i--)
+for (i--; i >= 0; i--)
 {
 free(grid[i]);
 }
