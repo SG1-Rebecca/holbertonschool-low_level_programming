e5d7f0ed7438ecc5c7ec8ad22eae3b4e4d8af7a5
Rebecca <rebecca.fsq011@protonmail.com>
2025-07-11 09:50:31 +0000

    fix Betty coding style

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -3,17 +3,18 @@
 #include <stddef.h>
 /**
 * malloc_checked - allocate memory using malloc
-* @b:
+* @b: number of bytes to allocate
 *
-* Return: pointer with status value of 98 if malloc fail
-*****/
+* Return: pointer to the allocated memory
+* If malloc fails, the program exits with status 98
+*/
 void *malloc_checked(unsigned int b)
 {
-  void *ptr;
-  ptr = malloc(b);
-    if (ptr == NULL)
-      {
-	exit(98);
-      }
-    return ptr;
+void *ptr;
+ptr = malloc(b);
+if (ptr == NULL)
+{
+exit(98);
+}
+return (ptr);
 }
