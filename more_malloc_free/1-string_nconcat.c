0ba43ff2b21f76e0f7b520a87dab268c12e643fe
Rebecca <rebecca.fsq011@protonmail.com>
2025-07-11 13:23:25 +0000

    fix betty coding and documentation style

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -5,8 +5,9 @@
 * string_nconcat - concatenate two strings
 * @s1: the first string
 * @s2: the second string
-* @n: number of bytes to allocates 
+* @n: number of bytes to allocates
 *
+* Return: pointer to a newly allocated string
 */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
@@ -25,24 +26,24 @@ for (length_s1 = 0; s1[length_s1] != '\0'; length_s1++)
 }
 for (length_s2 = 0; s2[length_s2] != '\0'; length_s2++)
 {
-  if (n >= length_s2)
-    {
-      n = length_s2;
-    }
+if (n >= length_s2)
+{
+n = length_s2;
 }
- new_str = malloc(sizeof(char) * (length_s1 + n + 1));
- if (new_str == NULL)
-   {
-     return (NULL);
-   }
- for (i = 0; i < length_s1; i++)
-   {
-     new_str[i] = s1[i];
-   }
- for (j = 0; i < length_s2; i++)
-   {
-     new_str[i + j] = s2[j];
-   }
+}
+new_str = malloc(sizeof(char) * (length_s1 + n + 1));
+if (new_str == NULL)
+{
+return (NULL);
+}
+for (i = 0; i < length_s1; i++)
+{
+new_str[i] = s1[i];
+}
+for (j = 0; i < length_s2; j++)
+{
+new_str[i + j] = s2[j];
+}
 new_str[i + j] = '\0';
 return (new_str);
 }
