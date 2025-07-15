fbc7c1d244cf575632b1ae1b62e69c281427870a
Rebecca <rebecca.fsq011@protonmail.com>
2025-07-15 10:40:17 +0000

    move size condition outside the loop

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -1,15 +1,24 @@
 #include "function_pointers.h"
-/*************/
+/**
+*
+*
+*
+*
+*
+******/
 int int_index(int *array, int size, int (*cmp)(int))
 {
   int i;
+  if (size <= 0)
+    {
+      return (-1);
+    }
   for (i = 0; i < size; i++)
     {
-      if (size <= 0)
+      if (cmp(array[i]))
 	{
-	  return (-1);
+	  return(i);
 	}
-      cmp(array[i]);
     }
-  return (0);
+  return (-1);
 }
