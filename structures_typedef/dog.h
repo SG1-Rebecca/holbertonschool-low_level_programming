7a1d028f2adbaf9c8709adfb5948de2a5ae6e6cf
Rebecca <rebecca.fsq011@protonmail.com>
2025-07-16 13:02:53 +0000

    fix small typo in documentation

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -5,7 +5,6 @@
 * @name: name of the dog
 * @age: age of the dog
 * @owner: owner of the dog
-*
 */
 struct dog
 {
@@ -13,5 +12,6 @@ char *name;
 float age;
 char *owner;
 };
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
