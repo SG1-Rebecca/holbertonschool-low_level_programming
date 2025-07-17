b77549f867ab9c14387f28c81d27bfe6a2494b64
Rebecca <rebecca.fsq011@protonmail.com>
2025-07-17 14:13:40 +0000

    fix betty documentation style

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -14,6 +14,13 @@ char *owner;
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+/**
+* struct dog_t - info about a dog
+* @name: name of the dog
+* @age: age of the dog
+* @owner: owner of the dog
+*
+*/
 typedef struct dog_t
 {
 char *name;
