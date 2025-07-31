018b79e81fc6c23cff8bc89e2e4dca86e7ed0eff
Rebecca F <rebecca.fsq011@protonmail.com>
2025-08-01 01:59:47 +0200

    draft

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stddef.h>
+#include <string.h>
+/**
+ * add_node - 
+ * @head:
+ * @str: string to be duplicated
+ * 
+ * Return: 
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+    list_t *new_node;
+    char *duplicated_str;
+
+    if (head == NULL || str == NULL)
+    {
+        return (NULL)
+    }
+
+ return (new_node);   
+}
