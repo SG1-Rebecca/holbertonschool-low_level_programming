bab09cf464e3ae5a338566a821a0791d27be81ba
Rebecca F <rebecca.fsq011@protonmail.com>
2025-08-02 14:59:06 +0200

    function that adds a new node at the end of a list

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,54 @@
+#include "lists.h"
+#include <string.h>
+#include <stdlib.h>
+/**
+ * add_node_end - adds a new node at the end of a list_t list
+ * @head: pointer to pointer to the head of the list
+ * @str: string to be duplicated
+ *
+ * Return: address of the new element or NULL if it failed
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+list_t *new_node, *end;
+char *duplicated_str;
+unsigned int length;
+
+if (head == NULL || str == NULL)
+{
+    return(NULL);
+}
+
+for (length = 0; str[length] != '\0'; length++)
+{
+}
+ duplicated_str = strdup(str);
+ if (duplicated_str == NULL)
+ {
+    return (NULL);
+ }
+ 
+ new_node = malloc(sizeof(list_t));
+ if (new_node == NULL)
+ {
+    free(duplicated_str);
+    return (NULL);
+ }
+
+ new_node->str = duplicated_str;
+ new_node->len = length;
+ new_node->next = NULL;
+
+ if (*head == NULL)
+ {
+    *head = new_node;
+    return (new_node);
+ }
+
+ for (end = *head; end->next != NULL; end= end->next)
+ {
+ }
+
+ end->next = new_node;
+ return (new_node);
+}
