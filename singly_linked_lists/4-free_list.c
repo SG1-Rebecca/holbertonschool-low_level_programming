ace5ef871bad96fe0ed9e01789b8aa35bf56fb2f
Rebecca F <rebecca.fsq011@protonmail.com>
2025-08-02 15:26:07 +0200

    function that frees a list_t list

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+#include <stddef.h>
+#include <stdlib.h>
+void free_list(list_t *head)
+{
+list_t *tmp;
+while (head != NULL)
+{
+    tmp = head;
+    head = head->next;
+    free(tmp->str);
+    free(tmp);
+}
+}
