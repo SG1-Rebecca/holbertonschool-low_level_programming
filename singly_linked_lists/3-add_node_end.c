48caf637df81c296f21646b1a6cb162113e06fb9
Rebecca F <rebecca.fsq011@protonmail.com>
2025-08-02 15:04:18 +0200

    fix betty coding && documentation style

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -13,42 +13,35 @@ list_t *add_node_end(list_t **head, const char *str)
 list_t *new_node, *end;
 char *duplicated_str;
 unsigned int length;
-
 if (head == NULL || str == NULL)
 {
-    return(NULL);
+return (NULL);
 }
-
 for (length = 0; str[length] != '\0'; length++)
 {
 }
- duplicated_str = strdup(str);
- if (duplicated_str == NULL)
- {
-    return (NULL);
- }
- 
- new_node = malloc(sizeof(list_t));
- if (new_node == NULL)
- {
-    free(duplicated_str);
-    return (NULL);
- }
-
- new_node->str = duplicated_str;
- new_node->len = length;
- new_node->next = NULL;
-
- if (*head == NULL)
- {
-    *head = new_node;
-    return (new_node);
- }
-
- for (end = *head; end->next != NULL; end= end->next)
- {
- }
-
- end->next = new_node;
- return (new_node);
+duplicated_str = strdup(str);
+if (duplicated_str == NULL)
+{
+return (NULL);
+}
+new_node = malloc(sizeof(list_t));
+if (new_node == NULL)
+{
+free(duplicated_str);
+return (NULL);
+}
+new_node->str = duplicated_str;
+new_node->len = length;
+new_node->next = NULL;
+if (*head == NULL)
+{
+*head = new_node;
+return (new_node);
+}
+for (end = *head; end->next != NULL; end = end->next)
+{
+}
+end->next = new_node;
+return (new_node);
 }
