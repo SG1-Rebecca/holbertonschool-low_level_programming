941af600cdecd07a893afd8f52c7100194440fc9
Rebecca F <rebecca.fsq011@protonmail.com>
2025-08-10 18:03:37 +0200

    fix betty coding style

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -2,24 +2,24 @@
 /**
  * cap_string - capitalizes all words of a string
  * @str: Pointer to the string
- * 
+ *
  * Return: Pointer to the modified string
  */
 char *cap_string(char *str)
 {
-int index; 
+int index;
 /*Capitalize first character if it's a lowercase*/
 if (str[0] >= 'a' && str[0] <= 'z')
 {
 str[0] -= 32;
-}    
+}
 for (index = 0; str[index] != '\0'; index++)
 {
 /*check for separators of words*/
 if (str[index] == '\t' || str[index] == '\n' || str[index] == ' ' ||
 str[index] == ',' || str[index] == ';' || str[index] == '.' ||
 str[index] == '!' || str[index] == '?' || str[index] == '"' ||
-str[index] == '(' || str[index] == ')' || str[index] == '{' || 
+str[index] == '(' || str[index] == ')' || str[index] == '{' ||
 str[index] == '}')
 {
 /* check if next character is lowercase */
@@ -27,7 +27,7 @@ if (str[index + 1] >= 'a' && str[index + 1] <= 'z')
 {
 str[index + 1] -= 32;
 }
-}    
+}
 }
 return (str);
 }
