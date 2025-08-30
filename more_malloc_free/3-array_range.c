e44cba82828b4c97d10fb88882cc162d18a0999a
Rebecca F <rebecca.fsq011@protonmail.com>
2025-08-30 21:56:41 +0200

    fix betty coding style

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -3,26 +3,26 @@
  * array_range - creates an array of integers
  * @min: min values
  * @max: max values
- * 
+ *
  * Return: the pointer to the newly created array
  */
 int *array_range(int min, int max)
 {
-    int *ptr, array_index, size;
-    if (min > max)
-    {
-        return (NULL);
-    }
-    size = max - min + 1;
-    ptr = malloc( size * sizeof(int));
-    if (ptr == NULL)
-    {
-        return(NULL);
-    }
-    /* Fill the array of int from min to max values*/
-    for (array_index = 0; array_index < size; array_index++)
-    {
-        ptr[array_index] = min + array_index;
-    }
-    return(ptr);
+int *ptr, array_index, size;
+if (min > max)
+{
+return (NULL);
+}
+size = max - min + 1;
+ptr = malloc(size *sizeof(int));
+if (ptr == NULL)
+{
+return (NULL);
+}
+/* Fill the array of int from min to max values*/
+for (array_index = 0; array_index < size; array_index++)
+{
+ptr[array_index] = min + array_index;
+}
+return (ptr);
 }
