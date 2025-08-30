49de9ee979051d60ba628865eb6a2af83b742120
Rebecca F <rebecca.fsq011@protonmail.com>
2025-08-30 22:06:48 +0200

    fix kernel style code

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -4,7 +4,7 @@
 /**
  * dlistint_len - returns the number of elements in a dlistint_t list
  * @h: pointer to the head of the list
- * 
+ *
  * Return: the number of elements in a linked list
  */
 size_t dlistint_len(const dlistint_t *h)
